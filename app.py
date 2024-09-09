6e04666262948151e3b62b098fb0efa65513c142
syifahyani <ahyanisyifa@gmail.com>
2024-09-09 18:31:20 +0800

    test

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ import pandas as pd
 import plotly.express as px
 
 app = Dash(__name__)
-server = app.server
-
-app.layout = html.H1('Hello World!')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-app = Dash(__name__)
 app.title = "MCM7183 Exercise 3"
 server = app.server
 
